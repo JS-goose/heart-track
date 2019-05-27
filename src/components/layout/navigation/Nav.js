fd6d50e96776eeb76a59369ad00c726a8a9504d0
Jonathan Sexton <joncsexton@gmail.com>
2019-05-27 17:17:12 -0500

    added content to log in section

--- src/components/layout/navigation/Nav.js
+++ src/components/layout/navigation/Nav.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from 'react';
 import styles from './Nav.module.css';
 
 export default function Nav() {
-    return (
-        <nav id={styles['nav-container']}>
-            <ul id={styles['nav-links-list']}>
-                <li>Home</li>
-                <li>About</li>
-                <li>Charts</li>
-                <li>Open Source</li>
-                <li>Contributors</li>
-            </ul>
+  return (
+    <nav id={styles['nav-container']}>
+      <ul id={styles['nav-links-list']}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Charts</li>
+        <li>Open Source</li>
+        <li>Contributors</li>
+      </ul>
 
-            <React.Fragment>Log In</React.Fragment>
-        </nav>
-    )
+      <React.Fragment>
+        <div id={styles['login-container']}>
+          <form action="">
+              <p>Log In</p>
+            <label htmlFor="{styles['username-input]}"></label>
+            <input id={styles['username-input']} type="text" placeholder="username" />
+            <label htmlFor="{styles['password-input']}"></label>
+            <input id={styles['password-input']} type="text" placeholder="password" />
+          </form>
+        </div>
+      </React.Fragment>
+    </nav>
+  );
 }
