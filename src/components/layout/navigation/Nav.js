dbbfdb33e6795704eb77ccb144badf0f70b3e32f
Jonathan Sexton <joncsexton@gmail.com>
2019-05-27 17:11:03 -0500

    added basic styling to nav and nav links

--- src/components/layout/navigation/Nav.js
+++ src/components/layout/navigation/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Nav.module.css';
 
 export default function Nav() {
     return (
-        <nav>
-            <ul id='nav-links-list'>
+        <nav id={styles['nav-container']}>
+            <ul id={styles['nav-links-list']}>
                 <li>Home</li>
                 <li>About</li>
                 <li>Charts</li>
