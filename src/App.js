c60e6b5077a53b624442837853fe87bd35f1b164
Jonathan Sexton <joncsexton@gmail.com>
2019-05-27 16:31:45 -0500

    added basic content to Nav and imported to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Nav from '../src/components/layout/navigation/Nav';
 
 function App() {
   return (
     <div className="App">
       Heart Track!
+      <Nav />
     </div>
   );
 }
--- src/components/layout/navigation/Nav.js
+++ src/components/layout/navigation/Nav.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Nav() {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul id='nav-links-list'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Charts</li>
+                <li>Open Source</li>
+                <li>Contributors</li>
+            </ul>
+        </nav>
     )
 }
