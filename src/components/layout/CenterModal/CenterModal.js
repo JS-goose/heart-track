ae56a34b7b49cec2a361ea6e6ba6b69a29902089
Jonathan Sexton <joncsexton@.com>
2019-06-04 15:37:05 -0500

    created basic modal with placeholder content

---
+++ src/components/layout/CenterModal/CenterModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CenterModal = () => {
+    return (
+        <article>
+            <h2>Heart Track</h2>
+            <p>Placeholder text Placeholder text</p>
+            <div>Demo --></div>
+        </article>
+    )
+}
+
+export default CenterModal;
