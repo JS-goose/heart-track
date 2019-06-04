8bf3b0bb4b960461b6ab450a3defadb600c6fd58
Jonathan Sexton <joncsexton@.com>
2019-06-04 15:42:01 -0500

    adjusted basic styling of modal

--- src/components/layout/CenterModal/CenterModal.js
+++ src/components/layout/CenterModal/CenterModal.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './CenterModal.module.css';
 
 const CenterModal = () => {
     return (
-        <article>
+        <article className={styles['centerModal']}>
             <h2>Heart Track</h2>
             <p>Placeholder text Placeholder text</p>
             <div>Demo --></div>
