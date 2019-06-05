5e430a40e5df5fa50b3470bf62ef9c6314972116
Jonathan Sexton <joncsexton@.com>
2019-06-05 15:56:13 -0500

    added FA icons

--- src/components/layout/navigation/Nav.js
+++ src/components/layout/navigation/Nav.js
@@ -1,25 +1,27 @@
 import React from "react";
 import styles from "./Nav.module.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import About from '../pages/About';
+import About from "../pages/About";
 
 export default function Nav() {
   return (
     <nav id={styles["nav-container"]}>
       <ul id={styles["nav-links-list"]}>
-      <li><Link to='/'>
-          Home
-        </Link></li>
-        <li><Link to='/about'>
-          About
-        </Link></li>
-        <li><Link to='/charts'>
-          Charts
-        </Link></li><li><Link to='/opensource'>
-          Open Source
-        </Link></li><li><Link to='/contributors'>
-          Contributors
-        </Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/charts">Charts</Link>
+        </li>
+        <li>
+          <Link to="/opensource">Open Source</Link>
+        </li>
+        <li>
+          <Link to="/contributors">Contributors</Link>
+        </li>
       </ul>
 
       <h1>❤HearTrack❤</h1>
@@ -29,8 +31,10 @@ export default function Nav() {
           <form action="">
             <p>Log In</p>
             <label htmlFor="{styles['username-input]}" />
+            <i class="fas fa-user-shield" />{" "}
             <input id={styles["username-input"]} type="text" placeholder="username" />
             <label htmlFor="{styles['password-input']}" />
+            <i class="fas fa-lock" />{" "}
             <input id={styles["password-input"]} type="password" placeholder="password" />
           </form>
         </div>
