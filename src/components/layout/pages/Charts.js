e50e106761cf1c643ab52b9f456c2c03563fd838
Jonathan Sexton <joncsexton@.com>
2019-06-05 16:38:51 -0500

    added basic form and input for user numbers

--- src/components/layout/pages/Charts.js
+++ src/components/layout/pages/Charts.js
@@ -1,15 +1,35 @@
 import React from "react";
-import placeholder from '../../../assets/imgs/chart-placeholder.jpg';
-import styles from './styles/Charts.module.css';
+import placeholder from "../../../assets/imgs/chart-placeholder.jpg";
+import styles from "./styles/Charts.module.css";
 
 const Charts = () => {
   return (
     <div>
       <h1>Charts</h1>
-      <img
-        src={placeholder}
-        alt="a chart graph showing red and green values"
-      />
+      <p>
+        Here users will eventually be able to manually input their vital statistic numbers and see
+        them charted out on their chosen time scale.
+      </p>
+      <img src={placeholder} alt="a chart graph showing red and green values" />
+      <form>
+      <label htmlFor={styles["heartRate-input"]} />
+      <input type="number" placeholder='heart rate'/>
+
+      <label htmlFor={styles["bp-systolic-input"]} />
+      <input type="number" placeholder='i.e. 120'/>
+
+      <label htmlFor={styles["bp-diastolic-input"]} />
+      <input type="number" placeholder='i.e. 80'/>
+
+      <label htmlFor={styles["weight-input"]} />
+      <input type="number" placeholder='weight'/>
+
+      <label htmlFor={styles["height-input"]} />
+      <input type="number" placeholder='height'/>
+
+      <label htmlFor={styles["age-input"]} />
+      <input type="number" placeholder='age'/>
+      </form>
     </div>
   );
 };
