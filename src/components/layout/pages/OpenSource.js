3f5813658829b2280ecd30dd8c06596f83ec5d69
Jonathan Sexton <joncsexton@.com>
2019-06-05 14:41:49 -0500

    established boilerplate for opensource page

---
+++ src/components/layout/pages/OpenSource.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OpenSource = () => {
+    return(
+        <div>
+            <h1>This is the OpenSource page.</h1>
+        </div>
+    )
+}
+
+export default OpenSource;
