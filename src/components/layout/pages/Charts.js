c9238cdc43053fb6906391504db82b5a5a96851e
Jonathan Sexton <joncsexton@.com>
2019-06-06 09:31:35 -0500

    added attribution for placeholder img

--- src/components/layout/pages/Charts.js
+++ src/components/layout/pages/Charts.js
@@ -10,25 +10,39 @@ const Charts = () => {
         Here users will eventually be able to manually input their vital statistic numbers and see
         them charted out on their chosen time scale.
       </p>
-      <img src={placeholder} alt="a chart graph showing red and green values" />
-      <form id={styles['user-inputs']}>
-      <label htmlFor={styles["heartRate-input"]} />
-      <input type="number" placeholder='heart rate'/>
 
-      <label htmlFor={styles["bp-systolic-input"]} />
-      <input type="number" placeholder='i.e. 120'/>
+      <figure>
+        <img src={placeholder} alt="a chart graph showing red and green values" />
+        <figcaption>
+          Photo by{" "}
+          <a href="https://unsplash.com/@m_b_m" target="_blank" rel="noreferrer noopener">
+            M. B. M.
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com" target="_blank" rel="noreferrer noopener">
+            Unsplash
+          </a>
+        </figcaption>
+      </figure>
 
-      <label htmlFor={styles["bp-diastolic-input"]} />
-      <input type="number" placeholder='i.e. 80'/>
+      <form id={styles["user-inputs"]}>
+        <label htmlFor={styles["heartRate-input"]} />
+        <input type="number" placeholder="heart rate" />
 
-      <label htmlFor={styles["weight-input"]} />
-      <input type="number" placeholder='weight'/>
+        <label htmlFor={styles["bp-systolic-input"]} />
+        <input type="number" placeholder="i.e. 120" />
 
-      <label htmlFor={styles["height-input"]} />
-      <input type="number" placeholder='height'/>
+        <label htmlFor={styles["bp-diastolic-input"]} />
+        <input type="number" placeholder="i.e. 80" />
 
-      <label htmlFor={styles["age-input"]} />
-      <input type="number" placeholder='age'/>
+        <label htmlFor={styles["weight-input"]} />
+        <input type="number" placeholder="weight" />
+
+        <label htmlFor={styles["height-input"]} />
+        <input type="number" placeholder="height" />
+
+        <label htmlFor={styles["age-input"]} />
+        <input type="number" placeholder="age" />
       </form>
     </div>
   );
