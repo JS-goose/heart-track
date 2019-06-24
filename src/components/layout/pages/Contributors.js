eb650782c178898ec6e7881a3b0a2dbed52c5783
Jonathan Sexton <joncsexton@.com>
2019-06-24 16:21:11 -0500

    added links to contributors

--- src/components/layout/pages/Contributors.js
+++ src/components/layout/pages/Contributors.js
@@ -1,21 +1,36 @@
 import React from "react";
-import styles from './styles/Contributors.module.css';
+import styles from "./styles/Contributors.module.css";
 
 const Contributors = () => {
   return (
-    <article className={styles['contributors-container']}>
+    <article className={styles["contributors-container"]}>
       <h1>Project Contributors</h1>
       <p>
         Here you will find a list of everyone that has helped in some form or fashion with the
         building of this project:
       </p>
-      <ul id={styles['contributors-list']}>
-        <li>Josep Lopez - @JosepLpez92</li>
+      <ul id={styles["contributors-list"]}>
         <li>
-          Dominic Xavier at The Art of Awe / @TheArtOfAwe /{" "}
-          <a href="https://theartofawe.co.uk" target="blank">
+          Josep Lopez /{" "}
+          <a href="https://twitter.com/JosepLpez92" target="_blank" rel="noopener noreferrer">
+            @JosepLpez92
+          </a>
+        </li>
+        <li>
+          Dominic Xavier at The Art of Awe /{" "}
+          <a href="https://twitter.com/TheArtOfAwe" target="_blank" rel="noopener noreferrer">
+            @TheArtOfAwe
+          </a>{" "}
+          /{" "}
+          <a href="https://theartofawe.co.uk" target="_blank" rel="noopener noreferrer">
             theheartofawe
           </a>
+        </li>
+        <li>
+          Jonathan Sexton /{" "}
+          <a href="https://twitter.com/jj_goose" target="_blank" rel="noopener noreferrer">
+            @jj_goose
+          </a> / <a href="https://jonathansexton.me" target="_blank" rel="noopener noreferrer">jonathansexton.me</a>
         </li>
       </ul>
     </article>
