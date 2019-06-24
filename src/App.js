c10859f8daaec65565e8abb5bc8f612f92065c1d
Jonathan Sexton <joncsexton@.com>
2019-06-24 17:40:48 -0500

    moved userinfo state to charts & converted to class

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
-import Nav from '../src/components/layout/navigation/Nav';
-import About from '../src/components/layout/pages/About'
-import Home from './components/layout/pages/Home'
-import Charts from './components/layout/pages/Charts'
-import OpenSource from './components/layout/pages/OpenSource'
-import Contributors from './components/layout/pages/Contributors'
+import "./App.css";
+import Nav from "../src/components/layout/navigation/Nav";
+import About from "../src/components/layout/pages/About";
+import Home from "./components/layout/pages/Home";
+import Charts from "./components/layout/pages/Charts";
+import OpenSource from "./components/layout/pages/OpenSource";
+import Contributors from "./components/layout/pages/Contributors";
 
 class App extends Component {
-  state = {
-    HR: 0,
-    BP: 0,
-    age: 0,
-    height: 0,
-    weight: 0
-  }
   render() {
     return (
       <Router>
-      <div className="App">
-        <Nav />
-        <Route exact path='/' component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/charts" component={Charts} />
-        <Route path="/contributors" component={Contributors} />
-        <Route path="/opensource" component={OpenSource} />
-      </div>
+        <div className="App">
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/charts" component={Charts} />
+          <Route path="/contributors" component={Contributors} />
+          <Route path="/opensource" component={OpenSource} />
+        </div>
       </Router>
     );
   }
--- src/components/layout/pages/Charts.js
+++ src/components/layout/pages/Charts.js
@@ -1,67 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import placeholder from "../../../assets/imgs/chart-placeholder.jpg";
 import styles from "./styles/Charts.module.css";
 
-const Charts = () => {
-  return (
-    <article id={styles["chart-container"]}>
-      <h1>Charts</h1>
-      <p>
-        Here users will eventually be able to manually input their vital statistic numbers and see
-        them charted out on their chosen time scale.
-      </p>
+class Charts extends Component {
+  state = {
+    HR: 0,
+    BP: 0,
+    age: 0,
+    height: 0,
+    weight: 0,
+  };
 
-      <form id={styles["user-inputs"]}>
-        <article>
-          <ul>
-            <li>
-              <label htmlFor={styles["heartRate-input"]}>Heart Rate: </label>
-              <input type="number" placeholder="heart rate" />
-            </li>
-            <li>
-              <label htmlFor={styles["bp-systolic-input"]}>BP - Systolic: </label>
-              <input type="number" placeholder="i.e. 120" />
-            </li>
-            <li>
-              <label htmlFor={styles["bp-diastolic-input"]}>BP - Diastolic: </label>
-              <input type="number" placeholder="i.e. 80" />
-            </li>
-          </ul>
-        </article>
+  render() {
+    return (
+      <article id={styles["chart-container"]}>
+        <h1>Charts</h1>
+        <p>
+          Here users will eventually be able to manually input their vital statistic numbers and see
+          them charted out on their chosen time scale.
+        </p>
 
-        <article>
-          <ul>
-            <li>
-              <label htmlFor={styles["weight-input"]}>Weight: </label>
-              <input type="number" placeholder="weight" />
-            </li>
-            <li>
-              <label htmlFor={styles["height-input"]}>Height: </label>
-              <input type="number" placeholder="height" />
-            </li>
-            <li>
-              <label htmlFor={styles["age-input"]}>Age: </label>
-              <input type="number" placeholder="age" />
-            </li>
-          </ul>
-        </article>
-      </form>
+        <form id={styles["user-inputs"]}>
+          <article>
+            <ul>
+              <li>
+                <label htmlFor={styles["heartRate-input"]}>Heart Rate: </label>
+                <input type="number" placeholder="heart rate" />
+              </li>
+              <li>
+                <label htmlFor={styles["bp-systolic-input"]}>BP - Systolic: </label>
+                <input type="number" placeholder="i.e. 120" />
+              </li>
+              <li>
+                <label htmlFor={styles["bp-diastolic-input"]}>BP - Diastolic: </label>
+                <input type="number" placeholder="i.e. 80" />
+              </li>
+            </ul>
+          </article>
 
-      <figure>
-        <img src={placeholder} alt="a chart graph showing red and green values" />
-        <figcaption>
-          Photo by{" "}
-          <a href="https://unsplash.com/@m_b_m" target="_blank" rel="noreferrer noopener">
-            M. B. M.
-          </a>{" "}
-          on{" "}
-          <a href="https://unsplash.com" target="_blank" rel="noreferrer noopener">
-            Unsplash
-          </a>
-        </figcaption>
-      </figure>
-    </article>
-  );
-};
+          <article>
+            <ul>
+              <li>
+                <label htmlFor={styles["weight-input"]}>Weight: </label>
+                <input type="number" placeholder="weight" />
+              </li>
+              <li>
+                <label htmlFor={styles["height-input"]}>Height: </label>
+                <input type="number" placeholder="height" />
+              </li>
+              <li>
+                <label htmlFor={styles["age-input"]}>Age: </label>
+                <input type="number" placeholder="age" />
+              </li>
+            </ul>
+          </article>
+        </form>
+
+        <figure>
+          <img src={placeholder} alt="a chart graph showing red and green values" />
+          <figcaption>
+            Photo by{" "}
+            <a href="https://unsplash.com/@m_b_m" target="_blank" rel="noreferrer noopener">
+              M. B. M.
+            </a>{" "}
+            on{" "}
+            <a href="https://unsplash.com" target="_blank" rel="noreferrer noopener">
+              Unsplash
+            </a>
+          </figcaption>
+        </figure>
+      </article>
+    );
+  }
+}
 
 export default Charts;
