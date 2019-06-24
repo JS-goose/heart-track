ad05ad8ff6f9849768003708d51f8715c805e568
Jonathan Sexton <joncsexton@.com>
2019-06-24 15:53:03 -0500

    added contributors to list

--- src/components/layout/pages/Contributors.js
+++ src/components/layout/pages/Contributors.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import styles from './Contributors.module.css';
 
 const Contributors = () => {
-    return(
-        <div>
-            <h1>This is the Contributors page.</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Project Contributors</h1>
+      <p>
+        Here you will find a list of everyone that has helped in some form or fashion with the
+        building of this project:
+      </p>
+      <ul id={styles['contributors-list']}>
+        <li>Josep Lopez - @JosepLpez92</li>
+        <li>
+          Dominic Xavier at The Art of Awe / @TheArtOfAwe /{" "}
+          <a href="https://theartofawe.co.uk" target="blank">
+            theheartofawe
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Contributors;
