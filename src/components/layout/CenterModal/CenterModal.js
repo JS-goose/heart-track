3632c6b63e121b8a68f960ac61f4ce8bdf21e345
Jonathan Sexton <joncsexton@.com>
2019-06-25 16:58:40 -0500

    working on modal layout lineup

--- src/components/layout/CenterModal/CenterModal.js
+++ src/components/layout/CenterModal/CenterModal.js
@@ -5,15 +5,17 @@ const CenterModal = () => {
   return (
     <article id={styles["home-landing-container"]}>
       <div className={styles["centerModal"]}>
-        <h2>Vital statistics data, charted out for you</h2>
+        <h1>Vital statistics data, charted out for you</h1>
         <p>
-          Enter your vitals, chart them, then export to a spreadsheet <br/>or share them with your
-          healthcare provider
+          Enter your vitals, chart them, then export to a spreadsheet <br />
+          or share them with your healthcare provider
         </p>
-        <button>
-          Demo <i className="fas fa-arrow-alt-circle-right" />
-        </button>
-        <button>Get Started</button>
+        <div id={styles["modal-button-container"]}>
+          <button>
+            Demo <i className="fas fa-arrow-alt-circle-right" />
+          </button>
+          <button>Get Started</button>
+        </div>
       </div>
     </article>
   );
