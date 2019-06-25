86afbb7ecb5a6517a08dcb640316fe984f372ad7
Jonathan Sexton <joncsexton@.com>
2019-06-25 14:25:13 -0500

    added wrapper around central modal

--- src/components/layout/CenterModal/CenterModal.js
+++ src/components/layout/CenterModal/CenterModal.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import styles from './CenterModal.module.css';
+import React from "react";
+import styles from "./CenterModal.module.css";
 
 const CenterModal = () => {
-    return (
-        <article className={styles['centerModal']}>
-            <h2>Heart Track</h2>
-            <p>Placeholder text Placeholder text</p>
-            <div>Demo <i className="fas fa-arrow-alt-circle-right"></i></div>
-        </article>
-    )
-}
+  return (
+    <article id={styles["home-landing-container"]}>
+      <div className={styles["centerModal"]}>
+        <h2>Heart Track</h2>
+        <p>Placeholder text Placeholder text</p>
+        <div>
+          Demo <i className="fas fa-arrow-alt-circle-right" />
+        </div>
+      </div>
+    </article>
+  );
+};
 
 export default CenterModal;
