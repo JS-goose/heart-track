3087bcdf85cfa76d9b5128dd7420fd6f929b2320
Jonathan Sexton <joncsexton@.com>
2019-07-10 15:52:28 -0500

    transitioned UserInputs to class component

--- src/components/layout/pages/Charts.js
+++ src/components/layout/pages/Charts.js
@@ -1,24 +1,9 @@
 import React, { Component } from "react";
-import UserInputs from './UserInputs';
+import UserInputs from "./UserInputs";
 import placeholder from "../../../assets/imgs/chart-placeholder.jpg";
 import styles from "./styles/Charts.module.css";
 
 class Charts extends Component {
-  state = {
-    HR: 0,
-    BP: 0,
-    age: 0,
-    height: 0,
-    weight: 0,
-  };
-
-  handleUserInput = (event) => {
-    console.log(event.target.valueAsNumber)
-    let updatedState = {...this.state};
-    updatedState = {HR: event.target.valueAsNumber};
-    this.setState({updatedState});
-  }
-
   render() {
     return (
       <article id={styles["chart-container"]}>
@@ -27,42 +12,6 @@ class Charts extends Component {
           Here users will eventually be able to manually input their vital statistic numbers and see
           them charted out on their chosen time scale.
         </p>
-
-        {/* <form id={styles["user-inputs"]}>
-          <article>
-            <ul>
-              <li>
-                <label htmlFor={styles["heartRate-input"]}>Heart Rate: </label>
-                <input type="number" placeholder="heart rate" onInput={this.handleUserInput}/>
-              </li>
-              <li>
-                <label htmlFor={styles["bp-systolic-input"]}>BP - Systolic: </label>
-                <input type="number" placeholder="i.e. 120" />
-              </li>
-              <li>
-                <label htmlFor={styles["bp-diastolic-input"]}>BP - Diastolic: </label>
-                <input type="number" placeholder="i.e. 80" />
-              </li>
-            </ul>
-          </article>
-
-          <article>
-            <ul>
-              <li>
-                <label htmlFor={styles["weight-input"]}>Weight: </label>
-                <input type="number" placeholder="weight" />
-              </li>
-              <li>
-                <label htmlFor={styles["height-input"]}>Height: </label>
-                <input type="number" placeholder="height" />
-              </li>
-              <li>
-                <label htmlFor={styles["age-input"]}>Age: </label>
-                <input type="number" placeholder="age" />
-              </li>
-            </ul>
-          </article>
-        </form> */}
         <UserInputs />
 
         <figure>
--- src/components/layout/pages/UserInputs.js
+++ src/components/layout/pages/UserInputs.js
@@ -1,42 +1,61 @@
-import React from "react";
-import styles from './styles/UserInputs.module.css';
+import React, { Component } from "react";
+import styles from "./styles/UserInputs.module.css";
 
-const UserInputs = (props) => (
-  <form id={styles["user-inputs"]}>
-    <article>
-      <ul>
-        <li>
-          <label htmlFor={styles["heartRate-input"]}>Heart Rate: </label>
-          <input type="number" placeholder="heart rate" onInput={props.handleUserInput} />
-        </li>
-        <li>
-          <label htmlFor={styles["bp-systolic-input"]}>BP - Systolic: </label>
-          <input type="number" placeholder="i.e. 120" />
-        </li>
-        <li>
-          <label htmlFor={styles["bp-diastolic-input"]}>BP - Diastolic: </label>
-          <input type="number" placeholder="i.e. 80" />
-        </li>
-      </ul>
-    </article>
+class UserInputs extends Component {
+  state = {
+    HR: 0,
+    BP: 0,
+    age: 0,
+    height: 0,
+    weight: 0,
+  };
 
-    <article>
-      <ul>
-        <li>
-          <label htmlFor={styles["weight-input"]}>Weight: </label>
-          <input type="number" placeholder="weight" />
-        </li>
-        <li>
-          <label htmlFor={styles["height-input"]}>Height: </label>
-          <input type="number" placeholder="height" />
-        </li>
-        <li>
-          <label htmlFor={styles["age-input"]}>Age: </label>
-          <input type="number" placeholder="age" />
-        </li>
-      </ul>
-    </article>
-  </form>
-);
+  handleUserInput = (event) => {
+    console.log(event.target.valueAsNumber);
+    let updatedState = { ...this.state };
+    updatedState = { HR: event.target.valueAsNumber };
+    this.setState({ updatedState });
+  };
 
-export default UserInputs
+  render() {
+    return (
+      <form id={styles["user-inputs"]}>
+        <article>
+          <ul>
+            <li>
+              <label htmlFor={styles["heartRate-input"]}>Heart Rate: </label>
+              <input type="number" placeholder="heart rate" onInput={this.handleUserInput} />
+            </li>
+            <li>
+              <label htmlFor={styles["bp-systolic-input"]}>BP - Systolic: </label>
+              <input type="number" placeholder="i.e. 120" />
+            </li>
+            <li>
+              <label htmlFor={styles["bp-diastolic-input"]}>BP - Diastolic: </label>
+              <input type="number" placeholder="i.e. 80" />
+            </li>
+          </ul>
+        </article>
+
+        <article>
+          <ul>
+            <li>
+              <label htmlFor={styles["weight-input"]}>Weight: </label>
+              <input type="number" placeholder="weight" />
+            </li>
+            <li>
+              <label htmlFor={styles["height-input"]}>Height: </label>
+              <input type="number" placeholder="height" />
+            </li>
+            <li>
+              <label htmlFor={styles["age-input"]}>Age: </label>
+              <input type="number" placeholder="age" />
+            </li>
+          </ul>
+        </article>
+      </form>
+    );
+  }
+}
+
+export default UserInputs;
